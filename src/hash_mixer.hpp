6f9f610088d332ff688eeacbf11deb41e86d64c4
ultramailman <cyrus9212@gmail.com>
2014-12-13 14:19:46 -0800

    prettied up the hash function

--- src/hash_mixer.hpp
+++ src/hash_mixer.hpp
@@ -1,16 +1,23 @@
 // Copyright (c) 2014, ultramailman
 // This file is licensed under the MIT License.
 
-#ifndef UNI_HASH_H
-#define UNI_HASH_H
+#ifndef HASH_MIXER_H
+#define HASH_MIXER_H
 #include "matrix_hash.hpp"
 #include <random>
 #include <vector>
+
+/* HashMixer is an example hash function for cuckoo hash table.
+Use this if you don't have a better parameterized hash function.
+*/
 template < class InType >
 class HashMixer{
 	std::vector< MatrixHash<unsigned, unsigned, std::mt19937 > > mats_;
 	std::hash<InType> hashf;
 public:
+	/* n is the number of functions.
+	seed is the seed of the random number generator.
+	*/
 	HashMixer(unsigned n, unsigned seed)
 	{
 		std::mt19937 g (seed);
--- src/matrix_hash.hpp
+++ src/matrix_hash.hpp
@@ -8,39 +8,60 @@ template <class InType>
 static bool parity(InType in)
 {
 	int bit = 0;
-	while(in) {
+	while(in)
+	{
 		bit ^= in & 1;
 		in >>= 1;
 	}
 	return bit != 0;
 }
+
+/* MatrixHash computes a hash by multiplying
+a randomly generated bit matrix on the input bit vector.
+*/
 template < class InType, class OutType, class Gen >
 class MatrixHash {
 	static constexpr unsigned cols = sizeof(InType) * CHAR_BIT;
 	static constexpr unsigned rows = sizeof(OutType) * CHAR_BIT;
+	
+	// the matrix is in column layout. (an array of column vectors)
 	OutType cols_ [cols];
-public:
-	MatrixHash (Gen & generator)
+	
+	// generate a bit string using the random generator
+	OutType genString(Gen & gen)
 	{
-		for(OutType & col: cols_) {
-			col = 0;
-	// initial every bit of the column with the parity of the generated number
-			for(unsigned i = 0; i < rows; i++) {
-				auto num = generator();
-				col |= parity(num);
-				col <<= 1;
-			}
+		OutType col = 0;
+		
+		for(unsigned i = 0; i < rows; i++)
+		{
+			auto num = gen();
+			col |= parity(num);
+			col <<= 1;
+		}
+		return col;
+	}
+public:
+	MatrixHash (Gen & gen)
+	{
+		// initialize the matrix with random bits
+		for(OutType & col: cols_)
+		{
+			col = genString(gen);
 		}
 	}
-	OutType operator () (InType k) const
+	
+	// compute bit matrix multiplication on bit vector.
+	OutType operator () (InType b) const
 	{
-		OutType sum = 0;
-		for(unsigned i = 0; i < cols; i++) {
-			if((k >> i) & 1) {
-				sum = sum ^ cols_[i];
-			}
+		OutType x = 0;
+		for(unsigned i = 0; i < cols; i++)
+		{
+			OutType bi = (b >> i) & 1;
+			OutType mask = 0 - bi;
+			OutType xi = cols_[i] & mask;
+			x = x ^ xi;
 		}
-		return sum;
+		return x;
 	}
 };
 #endif
